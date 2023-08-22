9298d7e49efbc31d7adb8b816bc81d7416c0fd47
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2023-08-22 16:37:32 -0500

    nuevos cambios estructura aun sin terminar

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {  } from "../../dist/output.css";
+
+const NavBar = () => {
+    return (
+        <div className='w-full h-14 bg-indigo-950 text-white'>
+            <h1>Hola mundo</h1>
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/Components/Reseña.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Reseña = () => {
+    return (
+        <div>
+            <section>
+                <img src="" alt="" />
+            </section>
+        </div>
+    );
+};
+
+export default Reseña;
